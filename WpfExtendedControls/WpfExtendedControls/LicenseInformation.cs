b3c2b3dcf7efa2703391f1182d6139dec04a33d2
Maxim <yugov.max@mail.ru>
2021-04-01 11:28:45 +0700

    Add comments and update project version

--- WpfExtendedControls/WpfExtendedControls/LicenseInformation.cs
+++ WpfExtendedControls/WpfExtendedControls/LicenseInformation.cs
@@ -10,6 +10,13 @@ namespace WpfExtendedControls
         public string Title { get; }
         public string License { get; }
         public bool IsTrial { get; }
+        /// <summary>
+        /// Create new object of license information
+        /// </summary>
+        /// <param name="title">License title</param>
+        /// <param name="license">License text</param>
+        /// <param name="isTrial">Trial flag, if license used in trial or it used in restricted mode
+        /// (for example: community licenses used in enterprise app and include restriction of this usage)</param>
         public LicenseInformation(string title, string license, bool isTrial)
         {
             Title = title;
